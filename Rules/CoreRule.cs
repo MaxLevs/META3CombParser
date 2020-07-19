77b3a9519e2f85e2697f36a69ffb38b9b21469b8
Maxim Liven <maxlevs@mail.ru>
2020-07-19 12:46:33 +0300

    Add operator for RuleConcatenate

--- Rules/CoreRule.cs
+++ Rules/CoreRule.cs
@@ -1,7 +1,14 @@
+using BFParser.Rules.Combinators;
+
 namespace BFParser.Rules
 {
     public abstract class CoreRule
     {
         public abstract SyntaxTreeNode Parse(string text);
+
+        public static CoreRule operator +(CoreRule firstRule, CoreRule secondRule)
+        {
+            return new RuleConcatenation(firstRule, secondRule);
+        } 
     }
 }

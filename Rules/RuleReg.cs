887f8c701d4c2e74f57b89aee493660fa5c5bd18
Maxim Liven <maxlevs@mail.ru>
2020-07-19 12:39:02 +0300

    Add basic realisation for terminal rules

--- Rules/RuleReg.cs
+++ Rules/RuleReg.cs
@@ -1,7 +1,24 @@
+using System.Text.RegularExpressions;
+
 namespace BFParser.Rules
 {
     public class RuleReg : CoreRule
     {
-        
+        public string Name { get; }
+        public Regex RExp { get; }
+
+        public RuleReg(string rule)
+        {
+            Name = $"Rule-[{rule}]";
+            RExp = new Regex(@$"^(\s+)*(?<ParsedText>{rule})(?<Rest>.+)*"); // Токен в начале строки, остаток в отдельной группе 
+        }
+
+        public override SyntaxTreeNode Parse(string text)
+        {
+            var pResult = RExp.Match(text);
+            var token = pResult.Groups["ParsedText"].Value;
+            var rest = pResult.Groups["Rest"].Value;
+            return new SyntaxTreeNode(token, rest, null);
+        }
     }
 }
--- Rules/RuleToken.cs
+++ Rules/RuleToken.cs
@@ -1,7 +1,24 @@
+using System.Text.RegularExpressions;
+
 namespace BFParser.Rules
 {
     public class RuleToken : CoreRule
     {
-        
+        public string Name { get; }
+        public Regex RExp { get; }
+
+        public RuleToken(string token)
+        {
+            Name = token;
+            RExp = new Regex(@$"^(\s+)*(?<Token>{token})(?<Rest>.+)*"); // Токен в начале строки, остаток в отдельной группе 
+        }
+
+        public override SyntaxTreeNode Parse(string text)
+        {
+            var pResult = RExp.Match(text);
+            var token = pResult.Groups["Token"].Value;
+            var rest = pResult.Groups["Rest"].Value;
+            return (pResult.Success) ? new SyntaxTreeNode(token, rest, null) : null;
+        }
     }
 }

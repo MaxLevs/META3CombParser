e52a867d8ad8eaa3683c21bbfdaefd19978f1021
Maxim Liven <maxlevs@mail.ru>
2020-07-19 12:40:07 +0300

    Test Realization

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using BFParser.Rules;
+using BFParser.Rules.Combinators;
 
 namespace BFParser
 {
@@ -6,7 +8,29 @@ namespace BFParser
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CoreRule ruleLogin = new RuleToken("login");
+            CoreRule ruleMaxlevs = new RuleToken("maxlevs");
+            CoreRule rulePasswd = new RuleToken("passwd");
+            CoreRule ruleKektus = new RuleToken("kektus");
+            
+            CoreRule rule = new RuleConcatenation(ruleLogin, ruleMaxlevs);
+            var node = rule.Parse(" login maxlevs passwd kektus");
+            
+            PrintR(node);
+        }
+
+        static void PrintR(SyntaxTreeNode node)
+        {
+            if (node is null)
+            {
+                Console.WriteLine("[NULL]");
+                return;
+            }
+            
+            Console.WriteLine(node.Id);
+            Console.WriteLine(node.ParsedText);
+            Console.WriteLine(node.Rest);
+            Console.WriteLine(node.Children);
         }
     }
 }

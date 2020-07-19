e2d34acda2a0f48c9aa9c7068caba2cef2d1e060
Maxim Liven <maxlevs@mail.ru>
2020-07-19 13:42:43 +0300

    Add OptionalRule

--- Rules/Combinators/RuleOptional.cs
+++ Rules/Combinators/RuleOptional.cs
@@ -2,9 +2,19 @@ namespace BFParser.Rules.Combinators
 {
     public class RuleOptional : CoreRule
     {
+        public CoreRule InternalRule { get; }
+        public string DefaultValue { get; }
+
+        public RuleOptional(CoreRule internalRule, string defaultValue = null)
+        {
+            InternalRule = internalRule;
+            DefaultValue = defaultValue;
+        }
+
         public override SyntaxTreeNode Parse(string text)
         {
-            throw new System.NotImplementedException();
+            var pResult = InternalRule.Parse(text);
+            return pResult ?? new SyntaxTreeNode(DefaultValue, text, null);
         }
     }
 }

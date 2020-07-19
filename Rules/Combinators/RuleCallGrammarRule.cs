df37d4e71dc915a74a59a8ca0e2140a31a756990
Maxim Liven <maxlevs@mail.ru>
2020-07-19 13:12:46 +0300

    Add Rule for Calling another grammar rules

---
+++ Rules/Combinators/RuleCallGrammarRule.cs
@@ -0,0 +1,12 @@
+namespace BFParser.Rules.Combinators
+{
+    public class RuleCallGrammarRule : CoreRule
+    {
+        public string GrammarRuleName { get; }
+        
+        public override SyntaxTreeNode Parse(string text)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

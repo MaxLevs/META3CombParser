8a939f8f3210433194650cd64faf6a4b80117ff0
Maxim Liven <maxlevs@mail.ru>
2020-07-22 12:38:30 +0300

    Make alternativeRule multi

--- Rules/Combinators/RuleAlternative.cs
+++ Rules/Combinators/RuleAlternative.cs
@@ -1,30 +1,58 @@
+using System;
+using System.Collections.Generic;
 using BFParser.Rules.DebugTools;
 
 namespace BFParser.Rules.Combinators
 {
     public class RuleAlternative : CoreRule
     {
-        public CoreRule FirstRule { get; }
-        public CoreRule SecondRule { get; }
+        public List<CoreRule> InnerRules { get; }
 
         public RuleAlternative(CoreRule firstRule, CoreRule secondRule)
         {
-            FirstRule = firstRule;
-            SecondRule = secondRule;
+            InnerRules = new List<CoreRule>();
+            CoreRule rule;
+            
+            rule = firstRule ?? throw new ArgumentNullException(nameof(firstRule));
+            if (rule is RuleAlternative alternativeRuleFirst)
+            {
+                InnerRules.AddRange(alternativeRuleFirst.InnerRules);
+            }
+            else
+            {
+                InnerRules.Add(rule);
+            }
+            
+            rule = secondRule ?? throw new ArgumentNullException(nameof(secondRule));
+            if (rule is RuleAlternative alternativeRuleSecond)
+            {
+                InnerRules.AddRange(alternativeRuleSecond.InnerRules);
+            }
+            else
+            {
+                InnerRules.Add(rule);
+            }
         }
 
         public override SyntaxTreeNode Parse(string text)
         {
-            var pResult = FirstRule.Parse(text) ?? SecondRule.Parse(text);
-            return pResult;
+            foreach (var rule in InnerRules)
+            {            
+                var pResult = rule.Parse(text);
+                if (pResult != null)
+                    return pResult;
+            }
+            return null;
         }
 
         public override Grammar Grammar { get; protected set; }
         public override void InitGrammar(Grammar grammar)
         {
             Grammar = grammar;
-            FirstRule.InitGrammar(grammar);
-            SecondRule.InitGrammar(grammar);
+            foreach (var innerRule in InnerRules)
+            {
+                innerRule.InitGrammar(grammar);
+            }
         }
         public override void Visit(CoreRuleVisitor visitor)
         {
